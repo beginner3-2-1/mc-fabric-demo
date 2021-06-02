63095b74b64edb03d1470cc1802bb47633ba62dc
beginner3 <465532252@qq.com>
2021-06-02 14:54:06 +0800

    烧制与合成

--- src/main/java/com/beginner321/demo/items/FirstItem.kt
+++ src/main/java/com/beginner321/demo/items/FirstItem.kt
@@ -1,7 +1,12 @@
 package com.beginner321.demo.items
 
 import com.beginner321.demo.Demo
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.world.World
 
 class FirstItem(settings: Settings) : Item(settings) {
 
@@ -10,5 +15,13 @@ class FirstItem(settings: Settings) : Item(settings) {
             .maxCount(30)
             .fireproof()
             .group(Demo.ITEM_GROUP))
+
+    override fun appendTooltip(stack : ItemStack? , world : World? , tooltip : MutableList<Text>? ,
+                               context : TooltipContext?)
+    {
+        tooltip?.add(TranslatableText("item.demo.first_item.tooltip",10))
+        tooltip?.add(TranslatableText("item.demo.first_item.tooltip_1"))
+        tooltip?.add(TranslatableText("item.demo.first_item.tooltip_2"))
+    }
 
 }
--- src/main/java/com/beginner321/demo/mixin/DeadMixin.java
+++ src/main/java/com/beginner321/demo/mixin/DeadMixin.java
@@ -2,9 +2,12 @@ package com.beginner321.demo.mixin;
 
 import com.alibaba.fastjson.JSON;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.tag.ItemTags;
@@ -28,7 +31,7 @@ public abstract class DeadMixin extends Entity{
     @Inject(method = "onDeath", at = @At(value = "RETURN"), cancellable = true)
     private void onDeathMixin(DamageSource source, CallbackInfo ci) {
         if(!world.isClient){
-            System.out.println(source.name);
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE,BeeEntity::new).dimensions(EntityDimensions.fixed(0.75f,0.75f));
             System.out.println(JSON.toJSONString(super.getType()));
         }
     }
